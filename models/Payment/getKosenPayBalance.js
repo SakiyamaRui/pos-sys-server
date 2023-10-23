eaafdde7010147e935bbc447553c50ea4e1210c6
SakiyamaRui <litogaming1110@gmail.com>
2023-10-23 17:58:02 +0900

    残高表示

--- models/Payment/getKosenPayBalance.js
+++ models/Payment/getKosenPayBalance.js
@@ -19,6 +19,7 @@ const getBalance = (
                 resolve(0);
             }
 
+            console.log(balance);
             resolve(result[0].balance || 0);
         }catch(e) {
             reject(e);
